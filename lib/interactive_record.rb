833e0748cb04edbb668d7b2274a3fd7d54a637fd
Chris Kerbo <chris.kerbo@outlook.com>
2019-10-31 20:27:31 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -57,7 +57,7 @@ def self.find_by(attribute)
   sql = <<-SQL
     SELECT *
     FROM #{self.table_name}
-    WHERE #{self.class.values_for_insert} = ?
+    WHERE #{self.values_for_insert} = ?
   SQL
   DB[:conn].execute(sql, attribute)
 end
